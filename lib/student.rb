5a3d7d40742d193c231bc667f8da1979d2e08fe0
Haley DeKeyser <hcd09@live.com>
2019-06-13 19:41:51 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -38,7 +38,7 @@ class Student
       WHERE grade = 9
       LIMIT 1;
     SQL
-    DB[:conn].execute(sql, grade).map do |row|
+    DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end
   end
